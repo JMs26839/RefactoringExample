57c96fa26d2ac50b7c43d44cf0fbaa6a4bb66a04
jakubmordal <s26839@pjwstk.edu.pl>
2024-04-04 21:04:38 +0200

    Final

--- zadanie/LegacyApp.tests/UserServiceTests.cs
+++ zadanie/LegacyApp.tests/UserServiceTests.cs
@@ -33,4 +33,159 @@ public class UserServiceTests
         //Assert
         Assert.Throws<ArgumentException>(action);
     }
+    
+    // // AddUser_ReturnsFalseWhenMissingAtSignAndDotInEmail
+// // AddUser_ReturnsFalseWhenYoungerThen21YearsOld
+// // AddUser_ReturnsTrueWhenVeryImportantClient
+// // AddUser_ReturnsTrueWhenImportantClient
+// // AddUser_ReturnsTrueWhenNormalClient
+// // AddUser_ReturnsFalseWhenNormalClientWithNoCreditLimit
+// // AddUser_ThrowsExceptionWhenUserDoesNotExist
+// // AddUser_ThrowsExceptionWhenUserNoCreditLimitExistsForUser
+
+
+
+    [Fact]
+    public void AddUser_ReturnsFalseWhenMissingAtSignAndDotInEmail()
+    {
+        //act
+        var userService = new UserService();
+        //arrenge
+        var res = userService.AddUser("John",
+            "smith",
+            "sthpl", 
+                     DateTime.Parse("10-10-2024"),
+            1);
+        //assert
+        Assert.False(res);
+
+    }
+    
+    [Fact]
+    public void AddUser_ReturnsFalseWhenYoungerThen21YearsOld()
+    {
+        //act
+        var userService = new UserService();
+        //arrenge
+        var res2 = userService.AddUser("John",
+            "smith",
+            "st@h.pl", 
+            DateTime.Parse("10-10-2024"),
+            1);
+        //assert
+        Assert.False(res2);
+
+    }
+    
+    
+    [Fact]
+    public void AddUser_ReturnsTrueWhenVeryImportantClient()
+    {
+        //act
+        var userService = new UserService();
+        //arrenge
+        var res2 = userService.AddUser("John",
+            "smith",
+            "st@h.pl", 
+            DateTime.Parse("10-10-2000"),
+            2);
+        //assert
+        Assert.True(res2);
+
+    }
+    
+    
+    [Fact]
+    public void AddUser_ReturnsTrueWhenImportantClient()
+    {
+        //act
+        var userService = new UserService();
+        //arrenge
+        var res2 = userService.AddUser("J",
+            "Smith",
+            "st@h.pl", 
+            DateTime.Parse("10-10-2000"),
+            3);
+        //assert
+        Assert.True(res2);
+
+    }
+    
+    
+    
+    [Fact]
+    public void AddUser_ReturnsFalseWhenNormalClientWithNoCreditLimit()
+    {
+        //act
+        var userService = new UserService();
+        //arrenge
+        var res3 = userService.AddUser("J",
+            "Kwiatkowski",
+            "kwiatkowski@wp.pl", 
+            DateTime.Parse("10-10-2002"),
+            5);
+        //assert
+        Assert.True(res3);
+
+    }
+    
+    
+    [Fact]
+    public void AddUser_ReturnsTrueWhenNormalClient()
+    {
+        //act
+        var userService = new UserService();
+        //arrenge
+        var res3 = userService.AddUser("J",
+            "Kwiatkowski",
+            "kwiatkowski@wp.pl", 
+            DateTime.Parse("10-10-2002"),
+            5);
+        //assert
+        Assert.True(res3);
+
+    }
+    
+    
+    [Fact]
+    public void  AddUser_ThrowsExceptionWhenUserDoesNotExist()
+    {
+        //Arange
+        var userService = new UserService();
+
+        //Act
+        Action action =()=> userService.AddUser(
+            "Joe",
+            "Smith",
+            "smith@page.com",
+            DateTime.Parse("2000-01-01"), 
+            100);
+
+        //Assert
+        Assert.Throws<ArgumentException>(action);
+    }
+    
+    // [Fact]
+    // public void AddUser_ThrowsExceptionWhenUserNoCreditLimitExistsForUser()
+    // {
+    //     // Arrange
+    //     var userService = new UserService();
+    //         
+    //     // Act
+    //     Action action = () => userService.AddUser(
+    //         "Jane", 
+    //         "Malewski", 
+    //         "malewski@gmail.pl", 
+    //         DateTime.Parse("1980-05-15"), 
+    //         clientId: 2
+    //     );
+    //         
+    //     // Assert
+    //     Assert.Throws<InvalidOperationException>(action);
+    // }
+    //
+    
+  
+
+
 }
--- zadanie/LegacyApp/UserService.cs
+++ zadanie/LegacyApp/UserService.cs
@@ -6,21 +6,11 @@ namespace LegacyApp
     {
         public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
         {
-            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
-            {
-                return false;
-            }
+            if (!firsLastNameNullOrEmpty(firstName, lastName)) return false;
 
             if (!Mail(email)) return false;
 
-            var now = DateTime.Now;
-            int age = now.Year - dateOfBirth.Year;
-            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
-
-            if (age < 21)
-            {
-                return false;
-            }
+            if (!AgeValidation(dateOfBirth)) return false;
 
             var clientRepository = new ClientRepository();
             var client = clientRepository.GetById(clientId);
@@ -34,6 +24,14 @@ namespace LegacyApp
                 LastName = lastName
             };
 
+            CreditValidation(client, user);
+
+            UserDataAccess.AddUser(user);
+            return true;
+        }
+
+        private static bool CreditValidation(Client client, User user)
+        {
             if (client.Type == "VeryImportantClient")
             {
                 user.HasCreditLimit = false;
@@ -56,13 +54,70 @@ namespace LegacyApp
                     user.CreditLimit = creditLimit;
                 }
             }
-
+        
             if (user.HasCreditLimit && user.CreditLimit < 500)
             {
                 return false;
             }
+        
+            return true;
+        }
+        
+        // private static void CreditValidation(Client client, User user)
+        // {
+        //     if (client.Type == "VeryImportantClient")
+        //     {
+        //         user.HasCreditLimit = false;
+        //     }
+        //     else
+        //     {
+        //         bool creditLimitSet = false;
+        //         user.HasCreditLimit = true;
+        //         using (var userCreditService = new UserCreditService())
+        //         {
+        //             int creditLimit = userCreditService.GetCreditLimit( user.LastName, user.DateOfBirth);
+        //             if (client.Type == "ImportantClient")
+        //             {
+        //                 creditLimit *= 2;
+        //             }
+        //
+        //             if (creditLimit < 500)
+        //             {
+        //                 throw new InvalidOperationException($"{client.Type} must have a credit limit of at least 500.");
+        //             }
+        //
+        //             user.CreditLimit = creditLimit;
+        //             creditLimitSet = true;
+        //         }
+        //
+        //         if (!creditLimitSet)
+        //         {
+        //             throw new InvalidOperationException("Failed to set credit limit for user.");
+        //         }
+        //     }
+        // }
 
-            UserDataAccess.AddUser(user);
+        private static bool AgeValidation(DateTime dateOfBirth)
+        {
+            var now = DateTime.Now;
+            int age = now.Year - dateOfBirth.Year;
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
+
+            if (age < 21)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool firsLastNameNullOrEmpty(string firstName, string lastName)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
+            {
+                return false;
+            }
+
             return true;
         }
 
